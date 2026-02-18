9c216765e766d2a5cc1fa09151aa6ce8d59007dd
Jay <thiscode@gmail.com>
2026-02-18 18:43:45 -0500

    fix: harden dashboard Dockerfile (pin bun version, escape sed, layer cache, healthz probes)

--- docker/docker-entrypoint.dashboard.sh
+++ docker/docker-entrypoint.dashboard.sh
@@ -2,5 +2,7 @@
 set -e
 MQTT_URL="${VITE_MQTT_URL:-wss://rabbitmq-ws.bto.bar/ws}"
 echo "Dashboard starting with MQTT URL: $MQTT_URL"
-find /usr/share/nginx/html -name "*.js" -exec sed -i "s|__MQTT_URL_PLACEHOLDER__|${MQTT_URL}|g" {} \;
+# Escape characters that sed treats as special in the replacement string (|, &, \)
+ESCAPED_URL=$(printf '%s\n' "$MQTT_URL" | sed 's/[|&\\]/\\&/g')
+find /usr/share/nginx/html -name "*.js" -exec sed -i "s|__MQTT_URL_PLACEHOLDER__|${ESCAPED_URL}|g" {} \;
 exec nginx -g "daemon off;"
